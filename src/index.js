fd1fcf1a942c81f6c3548050f36010206a709ab8
veronica-hottenroth <93673038+veronica-hottenroth@users.noreply.github.com>
2021-11-22 14:40:48 -0600

    Reorganized js

--- src/index.js
+++ src/index.js
@@ -1,54 +1,7 @@
-function showCity(event) {
-  event.preventDefault();
-  let cityInput = document.querySelector("#search-input");
-  let element = document.querySelector("#current-city");
-  element.innerHTML = `${cityInput.value}`;
-
-  let apiKey = "b0a3ae53d6b8668034340ca91d9b9f65";
-  let units = "imperial";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=${units}`;
-  console.log(apiUrl);
-  axios.get(apiUrl).then(showTemperature);
-}
-
-function showTemperature(response) {
-  console.log(response.data);
-  let temperature = Math.round(response.data.main.temp);
-  let city = response.data.name;
-  let max = Math.round(response.data.main.temp_max);
-  let min = Math.round(response.data.main.temp_min);
-  let humidity = Math.round(response.data.main.humidity);
-  let feelsLike = Math.round(response.data.main.feels_like);
-
-  let temp = document.querySelector("#current-temp");
-  let element = document.querySelector("#current-city");
-  let maxTemp = document.querySelector("#max-today");
-  let minTemp = document.querySelector("#min-today");
-  let humid = document.querySelector("#humidity-today");
-  let feels = document.querySelector("#feels-like");
-  let icon = document.querySelector("#icon");
-
-  temp.innerHTML = `${temperature}°F`;
-  element.innerHTML = city;
-  maxTemp.innerHTML = `High ${max}°F`;
-  minTemp.innerHTML = `Low ${min}°F`;
-  humid.innerHTML = `Humidity: ${humidity}%`;
-  feels.innerHTML = `Feels like ${feelsLike}°F`;
-  icon.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-}
-
-let searchButton = document.querySelector("#button-input");
-searchButton.addEventListener("click", showCity);
-
-let searchBar = document.querySelector("#search-input");
-searchBar.addEventListener("submit", showCity);
-
 // GET AND DISPLAY LOCAL TIME IN YOUR BROWSER
 
 let currentTime = new Date();
+
 function formatDate(date) {
   let currentHour = currentTime.getHours();
   if (currentHour < 10) {
@@ -74,6 +27,53 @@ function formatDate(date) {
   let day = days[currentDay];
   return `Last updated: ${day} ${currentHour}:${currentMinute}`;
 }
-let dateSubtitle = document.querySelector("#current-day");
 
-dateSubtitle.innerHTML = formatDate(currentTime);
+let lastUpdated = document.querySelector("#current-day");
+lastUpdated.innerHTML = formatDate(currentTime);
+
+function showCity(event) {
+  event.preventDefault();
+
+  let cityInput = document.querySelector("#search-input");
+  let element = document.querySelector("#current-city");
+  element.innerHTML = `${cityInput.value}`;
+
+  let apiKey = "b0a3ae53d6b8668034340ca91d9b9f65";
+  let units = "imperial";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let city = response.data.name;
+  let max = Math.round(response.data.main.temp_max);
+  let min = Math.round(response.data.main.temp_min);
+  let humidity = Math.round(response.data.main.humidity);
+  let feelsLike = Math.round(response.data.main.feels_like);
+
+  let tempElement = document.querySelector("#current-temp");
+  let cityElement = document.querySelector("#current-city");
+  let maxTempElement = document.querySelector("#max-today");
+  let minTempElement = document.querySelector("#min-today");
+  let humidElement = document.querySelector("#humidity-today");
+  let feelsElement = document.querySelector("#feels-like");
+  let iconElement = document.querySelector("#icon");
+
+  tempElement.innerHTML = `${temperature}°F`;
+  cityElement.innerHTML = city;
+  maxTempElement.innerHTML = `High ${max}°F`;
+  minTempElement.innerHTML = `Low ${min}°F`;
+  humidElement.innerHTML = `Humidity: ${humidity}%`;
+  feelsElement.innerHTML = `Feels like ${feelsLike}°F`;
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+}
+
+let searchButton = document.querySelector("#button-input");
+searchButton.addEventListener("click", showCity);
+
+let searchBar = document.querySelector("#search-input");
+searchBar.addEventListener("submit", showCity);
