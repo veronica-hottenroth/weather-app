ec0d6b02445cfb7ab031e9d09c5e30c8fc9c7c62
veronica-hottenroth <93673038+veronica-hottenroth@users.noreply.github.com>
2021-11-23 22:36:35 -0600

    Added forecast html, js, api

--- src/index.js
+++ src/index.js
@@ -24,6 +24,54 @@ function formatDate(date) {
   return `Last updated: ${day} ${currentHour}:${currentMinute}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function showForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#weather-forecast");
+  let forecastHTML = `<div class = "row mt-5">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
+            <div class = "col">
+              <div class = "weather-forecast-date mx-auto"> ${formatDay(
+                forecastDay.dt
+              )} </div>
+              <img class = "forecast-icon" src="http://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png" alt=""> </img> 
+              <div class = "weather-forecast-temperatures"> 
+                <span class = "weather-forecast-max"> ${Math.round(
+                  forecastDay.temp.max
+                )}°</span>
+                <span class = "weather-forecast-min">${Math.round(
+                  forecastDay.temp.min
+                )}°</span>
+              </div>
+            </div>
+      `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=b0a3ae53d6b8668034340ca91d9b9f65&units=imperial`;
+  axios.get(apiUrl).then(showForecast);
+  console.log(apiUrl);
+}
+
 function search(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b0a3ae53d6b8668034340ca91d9b9f65&units=imperial`;
   axios.get(apiUrl).then(showTemperature);
@@ -55,6 +103,8 @@ function showTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  console.log(response.data);
+  getForecast(response.data.coord);
 }
 
 function handleSubmit(event) {
